bfc10292b43728f8e0c427c6f660148b2a310974
Mohammad Abbasi <elanteria@gmail.com>
2019-10-19 12:52:41 +0330

    add project to git

---
+++ src/app/flash/flash.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { IFlash } from '../flash.model';
+
+@Component({
+    selector: 'app-flash',
+    templateUrl: './flash.component.html',
+    styleUrls: ['./flash.component.scss']
+})
+export class FlashComponent {
+    @Input() flash: IFlash = {
+        id: 1,
+        question: 'react to angular',
+        answer: 'No Reaction :)',
+        show: false
+    }
+}
