f3962cb643416db6a6dbce6e7d9665209f5a7439
Mohammad Abbasi <elanteria@gmail.com>
2019-10-20 22:16:51 +0330

    add delete,correct and incorrect functionality to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,9 @@ import { MathUtil } from './utilities/math.util';
 export class AppComponent {
     title = 'ng-flashcards';
     private digits: number = 5;
+    private editing: boolean = false;
+    private editId: number;
+
 
     flashs: IFlash[] = [
         { id: MathUtil.getRandomNumber(this.digits), answer: 'Answer 1', question: 'Question 1', show: false },
@@ -24,5 +27,31 @@ export class AppComponent {
     handleToggleCard(id: number) {
         const flash = this.flashs.find(f => f.id === id);
         flash.show = !flash.show;
+    }
+
+    handleEdit(id: number) {
+        this.editing = true;
+        this.editId = id;
+
+        //TODO: I will add editing logic after adding the form.
+    }
+
+    handleDelete(id: number) {
+        const flash = this.find(id);
+        if (flash) {
+            const index = this.flashs.indexOf(flash);
+            this.flashs.splice(index, 1);
+        }
+    }
+
+    handleRememberedChange({ id, flag }) {
+        const flash = this.find(id);
+        if (flash) {
+            flash.remembered = flag;
+        }
+    }
+
+    private find(id: number): IFlash {
+        return this.flashs.find(f => f.id == id);
     }
 }
